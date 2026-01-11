0315dd076f23db2eae482de0894c8fbbb6118343
Loukmane Djemaioune <loukmanedjemaioune@gmail.com>
2026-01-11 10:47:14 +0100

    initial commit

---
+++ Project/DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = /*ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;*/
+            "Server=.;Database=DVLD;User Id=sa;Password=NewStrongPassword123";
+
+
+    }
+}
